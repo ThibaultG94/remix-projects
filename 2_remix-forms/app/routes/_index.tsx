20ecff09026cab414176e5ac79aef32304829a3f
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-09-14 15:40:27 +0200

    Ajoute la gestion des formulaires avec validation Zod et intégration de Conform

--- 2_remix-forms/app/routes/_index.tsx
+++ 2_remix-forms/app/routes/_index.tsx
@@ -1,3 +1,5 @@
+import { getFormProps, getInputProps, useForm } from "@conform-to/react";
+import { getZodConstraint, parseWithZod } from "@conform-to/zod";
 import { ActionFunctionArgs } from "@remix-run/node";
 import { Form, json, useActionData } from "@remix-run/react";
 import { z } from "zod";
@@ -8,29 +10,46 @@ const Schema = z.object({
 });
 
 export const action = async ({ request }: ActionFunctionArgs) => {
- const formData = await request.formData();
- const object = Object.fromEntries(formData);
- const { email } = Schema.parse(object);
-  console.log(email);
+  const formData = await request.formData();
+  const submission = parseWithZod(formData, { schema: Schema })
 
-  const isValid = email === "tibo@guilty.fr";
-  return json({ isValid });
+  // Send the submisiion back to the client if the status is not successful
+  if (submission.status !== "success") return json({ result: submission.reply() });
+  const { email, name } = submission.value;
+  console.log({ email, name });
+
+  return json({ result: submission.reply() });
 };
 
 export default function Index() {
   const data = useActionData<typeof action>();
+  const [form, fields] = useForm({
+    constraint: getZodConstraint(Schema),
+    lastResult: data?.result,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: Schema,
+      })
+    },
+    shouldValidate: "onSubmit",
+  })
+
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <h1 className="leading text-2xl font-bold mb-4 text-gray-800 dark:text-gray-100">
         Welcome to Remix!
       </h1>
-      <Form className="text-white flex flex-col gap-2" method="POST">
-        <input type="email" className="text-black px-1" name="email" placeholder="Email" />
-        {data?.isValid === false && (
-          <p className="text-red-500">Email invalide</p>
-        )}
-        <input type="text" className="text-black px-1" name="name" minLength={3} placeholder="Nom" />
+      <Form className="text-white flex flex-col gap-2" method="POST" {...getFormProps(form)} >
+        <input className="text-black px-1" {...getInputProps(fields.email, {
+          type: "email",
+        })} placeholder="Email" />
+        <div className="text-red-600">{fields.email.errors}</div>
+        <input className="text-black px-1" {...getInputProps(fields.name, {
+          type: "text",
+        })} placeholder="Nom" />
+         <div className="text-red-600">{fields.name.errors}</div>
         <button className="border bg-teal-300 rounded text-black p-1">Soumettre</button>
+        <div className="text-red-600">{form.errors}</div>
       </Form>
     </div>
   );
