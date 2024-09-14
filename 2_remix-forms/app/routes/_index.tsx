ef6529e95d891e12693dfdf5d3e94984e287b2ec
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-09-14 14:36:10 +0200

    Ajoute une action pour le formulaire et impose une longueur minimale pour le champ de saisie

--- 2_remix-forms/app/routes/_index.tsx
+++ 2_remix-forms/app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import { Form } from "@remix-run/react";
 
+export const action = async () => {
+ return null;
+};
+
 export default function Index() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
@@ -7,7 +11,7 @@ export default function Index() {
         Welcome to Remix!
       </h1>
       <Form className="text-white" method="POST">
-        <input type="text" className="text-black" name="name" id="name" />
+        <input type="text" className="text-black" name="name" minLength={3} />
         <button className="border bg-teal-300 rounded text-black p-1 ml-2">Soumettre</button>
       </Form>
     </div>
