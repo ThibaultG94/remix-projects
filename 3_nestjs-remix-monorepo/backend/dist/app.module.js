0d11eec183fc17c7ec2769fc75401627d8c8dbfc
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-09-15 18:28:37 +0200

    Ajoute la configuration TypeScript et ESLint pour le monorepo, met à jour les fichiers package.json et ajuste les tsconfig pour le backend et le frontend

--- 3_nestjs-remix-monorepo/backend/dist/app.module.js
+++ 3_nestjs-remix-monorepo/backend/dist/app.module.js
@@ -7,17 +7,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
-const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-let AppModule = class AppModule {
-};
+var common_1 = require("@nestjs/common");
+var app_controller_1 = require("./app.controller");
+var app_service_1 = require("./app.service");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        (0, common_1.Module)({
+            imports: [],
+            controllers: [app_controller_1.AppController],
+            providers: [app_service_1.AppService],
+        })
+    ], AppModule);
+    return AppModule;
+}());
 exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
